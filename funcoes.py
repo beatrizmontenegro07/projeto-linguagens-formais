7c5c33238cbe82d3c678289a111a26c8598b5a0a
Ana Cecilia <anacecilia@q7.int.logq.dev>
2024-09-20 18:47:04 -0300

    passos 1 e 2 do algoritmo de conversão

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def readFile(arq):
     alfabeto = []
@@ -62,7 +63,7 @@ def readFile(arq):
 
     return alfabeto, estados, inicial, finais, transicoes
 
-def EhValido(alfabeto, estados, transicoes):
+def isValid(alfabeto, estados, transicoes):
     #a ideia é criar uma matriz de estados por alfabeto, que armazena quantas transições daquele simbolo do alfabeto tem nquele estado
     contagem_em_zero = [[0 for _ in range(len(alfabeto))] for _ in range(len(estados))]
 
@@ -75,7 +76,7 @@ def EhValido(alfabeto, estados, transicoes):
         matriz.loc[transicao['partida'], transicao['simbolo']] += 1
 
     #ao final dessa contagem, a matriz deve conter apenas 1s para ser considerada um AFD
-    #print(matriz)
+    print(matriz)
 
     #verificação dos valores da matriz
     AFD_valido = True
@@ -86,3 +87,61 @@ def EhValido(alfabeto, estados, transicoes):
                 break
     
     return AFD_valido
+
+
+def minimizationOfDFA(matriz_transicao, alfabeto, estados, inicial, finais):
+
+    #criando um dicionario que relaciona um estado a um indice, seguindo a ordem da lista "estados", a fim de consultá-lo para garantir o uso da matriz triangular de baixo
+    dic_indices = {}
+    index = 0
+    for e in estados:
+        dic_indices[e] = index
+        index += 1
+    
+
+    #criando um dicionario em que cada chave é um estado, e cada eh elemento é uma lista que representa a linha de cada matriz triangular
+    table = {}
+
+    for linha in estados:
+        lista = []
+        for coluna in estados:
+            if dic_indices[linha] > dic_indices[coluna]:
+                lista.append(0)
+        
+        if lista:
+            table[linha] = lista
+    
+    print("step 1")
+
+    for linha in table.values():
+        print(linha)
+    
+    #lista de nao finais
+    nao_finais = []
+    
+    for e in estados:
+        isFinal = False
+        for f in finais:
+            if f == e:
+                isFinal = True
+                break
+        if isFinal == False:
+            nao_finais.append(e)
+    
+
+    for f in finais:
+        for nf in nao_finais:
+            #como verificar se f e nf está na ordem da table, como garantir que stá fazendo na matriz triangular de baixo?
+            #podemos usar um map que relaciona um estado a uma ordem
+            if dic_indices[f] > dic_indices[nf]:
+                table[f][dic_indices[nf]] = 1
+            else:
+                table[nf][dic_indices[f]] = 1
+    
+    print("step 2")
+    for linha in table.values():
+        print(linha)
+
+    return
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from funcoes import *
+import numpy as np
+import pandas as pd
             
 #Main
 
@@ -14,12 +16,31 @@ try:
     
 except FileNotFoundError:
     print("Erro: O arquivo não foi encontrado")
+    exit(0)
 except PermissionError:
     print("Erro: Não foi possível abrir o arquivo")
+    exit(0)
 except Exception as e:
     print(f"Erro: {e}")
+    exit(0)
 
 
-valido = EhValido(alfabeto, estados, transicoes)
+valido = isValid(alfabeto, estados, transicoes)
 
-print(valido)
+if valido:
+    print("AFD é valido")
+else:
+    print("Autômato não é um AFD")
+    exit(0)
+
+#fazendo uma matriz de transição no formato Q X ALFABETO
+
+matriz_transicao = pd.DataFrame(np.nan, estados, alfabeto)
+
+for t in transicoes:
+    matriz_transicao.loc[t['partida'], t['simbolo']] = t['chegada']
+
+print("Matrix de transição")
+print(matriz_transicao)
+
+minimizationOfDFA(matriz_transicao, alfabeto, estados, inicial, finais)
