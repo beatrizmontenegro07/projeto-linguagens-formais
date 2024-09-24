42809792af7c6ad80e00a243e60a35d37df359e8
beatrizmontenegro07 <beatriz.m.maia07@gmail.com>
2024-09-23 22:01:45 -0300

    ajustes

--- main.py
+++ main.py
@@ -13,34 +13,22 @@ try:
     print(inicial)
     print(finais)
     print(transicoes)
+
+    #fazendo uma matriz de transição no formato Q X ALFABETO
+
+    matriz_transicao = pd.DataFrame(np.nan, estados, alfabeto, dtype=object)
+
+    for t in transicoes:
+        matriz_transicao.loc[t['partida'], t['simbolo']] = t['chegada']
+
+    print("Matrix de transição")
+    print(matriz_transicao)
+
+    minimizationOfDFA(matriz_transicao, alfabeto, estados, inicial, finais)
     
 except FileNotFoundError:
     print("Erro: O arquivo não foi encontrado")
-    exit(0)
 except PermissionError:
     print("Erro: Não foi possível abrir o arquivo")
-    exit(0)
 except Exception as e:
     print(f"Erro: {e}")
-    exit(0)
-
-
-valido = isValid(alfabeto, estados, transicoes)
-
-if valido:
-    print("AFD é valido")
-else:
-    print("Autômato não é um AFD")
-    exit(0)
-
-#fazendo uma matriz de transição no formato Q X ALFABETO
-
-matriz_transicao = pd.DataFrame(np.nan, estados, alfabeto)
-
-for t in transicoes:
-    matriz_transicao.loc[t['partida'], t['simbolo']] = t['chegada']
-
-print("Matrix de transição")
-print(matriz_transicao)
-
-minimizationOfDFA(matriz_transicao, alfabeto, estados, inicial, finais)
